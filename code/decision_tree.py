2c3de90ecef2a8150f61eecb95390192a4b32700
Archit Rathore <architrathore1@gmail.com>
2017-09-08 18:48:01 -0600

    Fix multipart names, unicode character in names

--- code/decision_tree.py
+++ code/decision_tree.py
@@ -6,17 +6,21 @@ Author: Archit Rathore
 class DataRecord:
     '''Class to store individual data points read from train and test files'''
 
-    def __init__(self, label, firstname, lastname):
-        self.label = 1 if label = '+' else 0
-        self.firstname = firstname
-        self.lastname = lastname
+    def __init__(self, label, name):
+        self.label = 1 if label == '+' else 0
+        self.name = name
+
+    def __repr__(self):
+        return str(self.label) + ' ' + ' '.join(self.name)
 
 def read_dataset(filepath):
     '''Read train or test file and return a list DataRecord objects'''
     data = []
-	with open(filepath, 'r') as f:
+    with open(filepath, 'r', encoding='utf-8') as f:
         for line in f:
-            label, fname, lname = line.rstrip().split()
-            record = DataRecord(label, fname, lname)
-            data.append(record)
+            line = line.rstrip().split()
+            label, name = line[0], line[1:]
+            data.append(DataRecord(label, name))
+    return data
 
+print(read_dataset('../Dataset/updated_train.txt')[0:3])
