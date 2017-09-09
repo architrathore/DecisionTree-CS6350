ab3ae5a7941720f95ad759b13dcbde2c5234ec0f
Archit Rathore <architrathore1@gmail.com>
2017-09-08 18:30:25 -0600

    Read input data

--- code/decision_tree.py
+++ code/decision_tree.py
@@ -2,3 +2,21 @@
 Implementation of decision tree for Machine Learning-CS6350
 Author: Archit Rathore
 '''
+
+class DataRecord:
+    '''Class to store individual data points read from train and test files'''
+
+    def __init__(self, label, firstname, lastname):
+        self.label = 1 if label = '+' else 0
+        self.firstname = firstname
+        self.lastname = lastname
+
+def read_dataset(filepath):
+    '''Read train or test file and return a list DataRecord objects'''
+    data = []
+	with open(filepath, 'r') as f:
+        for line in f:
+            label, fname, lname = line.rstrip().split()
+            record = DataRecord(label, fname, lname)
+            data.append(record)
+
